dc6387fcddaf525346f6062ce887282a4b8e63de
novakf <bogdannovikov116@mail.ru>
2024-01-29 21:12:38 +0300

    token update

--- src/App/constants.ts
+++ src/App/constants.ts
@@ -1,8 +1,8 @@
 import CryptoJS from 'crypto-js';
 
 const getToken = () => {
-  let decoded = 'U2FsdGVkX1+Apct6WKtvwpp5wD+ADOEkS+mSUtayl5zuj3tFobn43n7i/dQmn2vItLg1MBI2AQfyoAcOsiSiUw==';
-  let bytes = CryptoJS.AES.decrypt(decoded, 'jhonsnow');
+  let decoded = 'ENCYPTED_TOKEN';
+  let bytes = CryptoJS.AES.decrypt(decoded, 'KEY');
   let GITHUB_API_TOKEN = bytes.toString(CryptoJS.enc.Utf8);
   return GITHUB_API_TOKEN;
 };
